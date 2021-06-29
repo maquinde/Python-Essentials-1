8eff84d4954fe7aa72ec60a5911839254072da01
Mike <maquinde@users.noreply.github.com>
2021-06-28 19:33:11 -0700

    Completed module 1

---
+++ module1/m1.py
@@ -0,0 +1,15 @@
+#Module 1
+
+# IL = instruction list
+# Natural Language
+from io import open_code
+
+
+# source code = program written in high level language
+# source file = file containing the source code
+
+# compilation = source program is translated once (compiled)
+# interpretation = transilates source program each time it has to be run (interpreter)
+print("Hisssssss.....")
+print("rawr")
+print("oink")
